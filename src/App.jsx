7dc142594c8aead00e62cc5f505274d12a1ca200
Shanoor24 <77038788+Shanoor24@users.noreply.github.com>
2024-09-08 15:00:48 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import './App.css';
+import Counter from './components/Counter';
+
+function App() {
+  return (
+    <>
+      <h1>Redux Example</h1>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
